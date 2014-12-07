31d2aac1ca20577bafa1f9241cba963c639b9747
Rafael Oliveira <ludug3r0@gmail.com>
2014-12-07 13:31:18 +0000

    Using cljx files so that we can tackle clojure and clojurescript codebases
    - Pending work on migrating tests from expactations to something that can test clojurescript (clojurescript.test maybe?)
    - Bumping project minor version so that other projects can still work if something goes wrong deploying at clojars

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject org.clojars.ludug3r0/go-schema "0.0.1-SNAPSHOT"
-            :description "A set of schemas and utility function to operate on go data structures"
+(defproject org.clojars.ludug3r0/go-schema "0.0.2-SNAPSHOT"
+            :description "A set of schemas and utility function to operate on cljx.go data structures"
             :url "https://github.com/ludug3r0/go-schema"
             :license {:name "Eclipse Public License"
                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
@@ -7,5 +7,16 @@
                            [prismatic/schema "0.3.3"]]
             :scm {:name "git"
                   :url  "https://github.com/ludug3r0/go-schema"}
-            :profiles {:dev        {:dependencies [[expectations "2.0.9"]]
-                                    :plugins [[lein-expectations "0.0.7"]]}})
+            :profiles {:dev {:dependencies [[expectations "2.0.9"]]
+                             :plugins      [[com.keminglabs/cljx "0.4.0"]
+                                            [lein-expectations "0.0.7"]]}}
+            :source-paths ["target/generated/src/clj"]
+            :test-paths ["test/clj"]
+            :cljx {:builds [{:source-paths ["src/cljx"]
+                             :output-path  "target/generated/src/clj"
+                             :rules        :clj}
+                            {:source-paths ["src/cljx"]
+                             :output-path  "target/generated/src/cljs"
+                             :rules        :cljs}]}
+            :prep-tasks   [["with-profile" "+dev" ; Workaround for :dev cljx
+                            "cljx" "once"]])
